2634387f53ba71b3c8611501e895529bb67de1dd
pablofolive <juliafonseca1412@gmail.com>
2026-05-29 14:46:28 -0300

    fdgh

---
+++ atividade-avaliativa-jogos/Assets/Bala-Codigo.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Bala-Código : MonoBehaviour
+{
+    public float velocidade = 20f;
+    public float tempoVida = 3f; // 
+
+    void Start()
+    {
+        
+        Destroy(gameObject, tempoVida);
+    }
+
+    void Update()
+    {
+        
+        transform.Translate(Vector3.forward * velocidade * Time.deltaTime);
+    }
+
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        Destroy(gameObject);
+    }
+}
+}
